cbd0cd2b489dfea128bd4417c70db5e65636188f
Brendan Manda <mandab@tcd.ie>
2022-04-09 21:59:44 +0100

    From the Book

---
+++ src/TST.java
@@ -0,0 +1,89 @@
+//https://algs4.cs.princeton.edu/52trie/TST.java.html
+//Code based from Algorithms 4th Edition by Robert Sedgewick and Kevin Wayne
+
+public class TST<Value> {
+    private int n;              // size
+    private Node<Value> root;   // root of TST
+
+    private static class Node<Value> {
+        private char c;                        // character
+        private Node<Value> left, mid, right;  // left, middle, and right subtries
+        private Value val;                     // value associated with string
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean contains(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    public Value get(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with null argument");
+        }
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node<Value> x = get(root, key, 0);
+        if (x == null) return null;
+        return x.val;
+    }
+
+    // return subtrie corresponding to given key
+    private Node<Value> get(Node<Value> x, String key, int d) {
+        if (x == null) return null;
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        char c = key.charAt(d);
+        if (c < x.c) return get(x.left, key, d);
+        else if (c > x.c) return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid, key, d + 1);
+        else return x;
+    }
+
+    public void put(String key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with null key");
+        }
+        if (!contains(key)) n++;
+        else if (val == null) n--;       // delete existing key
+        root = put(root, key, val, 0);
+    }
+
+    private Node<Value> put(Node<Value> x, String key, Value val, int d) {
+        char c = key.charAt(d);
+        if (x == null) {
+            x = new Node<Value>();
+            x.c = c;
+        }
+        if (c < x.c) x.left = put(x.left, key, val, d);
+        else if (c > x.c) x.right = put(x.right, key, val, d);
+        else if (d < key.length() - 1) x.mid = put(x.mid, key, val, d + 1);
+        else x.val = val;
+        return x;
+    }
+
+    public String longestPrefixOf(String query) {
+        if (query == null) {
+            throw new IllegalArgumentException("calls longestPrefixOf() with null argument");
+        }
+        if (query.length() == 0) return null;
+        int length = 0;
+        Node<Value> x = root;
+        int i = 0;
+        while (x != null && i < query.length()) {
+            char c = query.charAt(i);
+            if (c < x.c) x = x.left;
+            else if (c > x.c) x = x.right;
+            else {
+                i++;
+                if (x.val != null) length = i;
+                x = x.mid;
+            }
+        }
+        return query.substring(0, length);
+    }
+
+}
